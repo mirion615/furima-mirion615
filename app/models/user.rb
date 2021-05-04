9b146944de6eb677015568e30d360345108011b5
mirion615 <mirion615@gmail.com>
2021-05-04 22:11:03 +0900

    passwordのvalidates修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,}\z/
-  
   validates :password,:password_confirmation,length:{minimum:6},
-              format:{with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers"}
+              format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: "Include both letters and numbers"}
   validates :nickname,         presence:true, length:{maximum:40}
   validates :family_name,:first_name, presence:true, 
               format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
