d57483d55a326eed8ec4dd2df831418d82fec9fe
mirion615 <mirion615@gmail.com>
2021-05-05 11:52:23 +0900

    バリデーションの修正
    with_optionを使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :password,:password_confirmation,length:{minimum:6},
-              format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: "Include both letters and numbers"}
-  validates :nickname,         presence:true, length:{maximum:40}
-  validates :family_name,:first_name, presence:true, 
-              format: {with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: "Full-width characters"}
-  validates :family_name_kana, :first_name_kana, presence:true,
-              format: { with: /\A([ァ-ン]|ー)+\z/, message: "is must NOT contain any other characters than alphanumerics." }
-  validates :birth_date,       presence:true
+
+  with_options presence: true do
+    validates :birth_date
+    validates :nickname, length:{maximum:40}
+
+    with_options length: {minimum:6}, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: "Include both letters and numbers"} do
+    validates :password
+    validates :password_confirmation
+    end
+    
+
+    with_options format: {with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: "Full-width characters"} do
+      validates :family_name
+      validates :first_name
+    end
+    
+    with_options format: { with: /\A([ァ-ン]|ー)+\z/, message: "is must NOT contain any other characters than alphanumerics." } do
+      validates :family_name_kana 
+      validates :first_name_kana
+    end
+  end
 end
