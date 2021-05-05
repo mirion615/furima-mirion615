4ea9a4de00eb115bea94311479fd8489dae86de7
mirion615 <mirion615@gmail.com>
2021-05-05 15:43:17 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana,
+                                             :birth_date])
   end
 
   def basic_auth
