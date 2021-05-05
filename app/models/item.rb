14ef148baa562a24ae4cc256bd20dbf6426d4ec1
mirion615 <mirion615@gmail.com>
2021-05-05 17:07:22 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,16 @@
 class Item < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :item_condition, :delivery_charge, :ship_from, :days_to_ship
   belongs_to :user
   has_one_attached :image
-end
+
+
+
+
+  with_options presence:true, numericality: { other_than: 1 } do
+    validates :category_id, :item_condition, :delivery_charge, :ship_from, :days_to_ship
+  end
+
+
+
+  end
--- db/migrate/20210505065218_create_items.rb
+++ db/migrate/20210505065218_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string     :title, null: false
       t.text       :text, null: false
-      t.integer   :category_id, null: false
+      t.integer   :category_id, null: false, 
       t.integer   :item_condition_id, null: false
       t.integer   :delivery_charge_id, null: false
       t.integer   :ship_from_id, null: false
