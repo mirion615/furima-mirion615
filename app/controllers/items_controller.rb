8ce7b9b1ada674c0ab1cf09ddf1fedee69f44a3b
mirion615 <mirion615@gmail.com>
2021-05-06 14:33:34 +0900

    テストコード

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
   private
   def item_params
-    params.require(:item).permit(:title, :text, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :title, :text, :item_price, :category_id, :item_condition_id, :delivery_charge_id, :ship_from_id, :days_to_ship_id).merge(user_id: current_user.id)
   end
 end
