534b1255ff90b26cb39bc9df1174dc61824a5661
mirion615 <mirion615@gmail.com>
2021-05-06 15:51:38 +0900

    不要な記述を削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,16 +2,12 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
 
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-    console.log(addTaxDom);
 
     const calProfit = document.getElementById("profit");
       const valueResult = inputValue * 0.1
-      console.log(valueResult);
       calProfit.innerHTML = (Math.floor(inputValue - valueResult));
-      console.log(calProfit)
   })
 });
