c9efeafac484360d94b58796f8bba8bba938a7ea
mirion615 <mirion615@gmail.com>
2021-05-06 17:37:17 +0900

    プッシュ

--- db/migrate/20210504031616_rename_faimily_name_kana_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameFaimilyNameKanaColumnToUsers < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :users, :faimily_name_kana, :family_name_kana
-  end
-end
--- db/migrate/20210506014854_rename_item_price_id_column_to_items.rb
+++
@@ -1,5 +0,0 @@
-class RenameItemPriceIdColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :item_price_id, :item_price
-  end
-end
