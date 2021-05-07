de8cdbea7a066193c7be0d5d7edb1bbd3cbacbd5
mirion615 <mirion615@gmail.com>
2021-05-07 12:37:07 +0900

    コントローラー
    ・createアクション定義
    ・move_to_index
    ・autenticate_user!

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: :edit
+
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -24,6 +28,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
 
 
   private
@@ -31,5 +44,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :title, :text, :item_price, :category_id, :item_condition_id, :delivery_charge_id,
                                  :ship_from_id, :days_to_ship_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
