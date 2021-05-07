14534171fac6985be29207d809a343aa51eb719d
mirion615 <mirion615@gmail.com>
2021-05-07 12:55:10 +0900

    updateアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, only: [:edit,:update]
 
 
   def index
