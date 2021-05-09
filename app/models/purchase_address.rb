757e1f4accf3930c06a6b23151c9128bc0e39ad7
mirion615 <mirion615@gmail.com>
2021-05-09 12:47:40 +0900

    フォームの情報を保存できるようにする

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -13,7 +13,7 @@ class PurchaseAddress
   validates :ship_from_id, numericality: {other_than: 1, message: "Select"}
 
   def save
-    purchase = Purchase.create(:user_id, item_id)
-    address = Address.create(postal_code: postal_code, city: city, house_number: house_number, phone_number: phone_number, ship_from_id: ship_from_id, purchase_id: purchase_id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    address = Address.create(postal_code: postal_code, city: city, house_number: house_number,building_name: building_name, phone_number: phone_number, ship_from_id: ship_from_id, purchase_id: purchase.id)
   end
 end
