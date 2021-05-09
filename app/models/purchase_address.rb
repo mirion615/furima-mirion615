bf584fc4c1347b97bc353f930b48f58001249f5a
mirion615 <mirion615@gmail.com>
2021-05-09 10:10:32 +0900

    Formオブジェクト作成
    .purchase_addressモデル

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,19 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor: [:postal_code, :ship_from_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id]
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
+    validates :city
+    validates :house_number
+    validates :phone_number, format { with:/^0\d{9,10}$/, message: 'Input only number' }
+    validates :user_id
+    validates :item_id
+  end
+  validates :ship_from_id, numericality: {other_than: 1, message: "Select"}
+
+  def save
+    purchase = Purchase.create(:user_id, item_id)
+    address = Address.create(postal_code: postal_code, city: city, house_number: house_number, phone_number: phone_number, ship_from_id: ship_from_id, purchase_id: purchase,id)
+  end
+end
