243ae2f7f225b055cae6eb89b219cf8ba338a897
mirion615 <mirion615@gmail.com>
2021-05-09 10:59:09 +0900

    purchase_addressモデル修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,12 +1,12 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor: [:postal_code, :ship_from_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id]
+  attr_accessor :postal_code, :ship_from_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
     validates :city
     validates :house_number
-    validates :phone_number, format { with:/^0\d{9,10}$/, message: 'Input only number' }
+    validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/, message: 'Input only number' }
     validates :user_id
     validates :item_id
   end
@@ -14,6 +14,6 @@ class PurchaseAddress
 
   def save
     purchase = Purchase.create(:user_id, item_id)
-    address = Address.create(postal_code: postal_code, city: city, house_number: house_number, phone_number: phone_number, ship_from_id: ship_from_id, purchase_id: purchase,id)
+    address = Address.create(postal_code: postal_code, city: city, house_number: house_number, phone_number: phone_number, ship_from_id: ship_from_id, purchase_id: purchase_id)
   end
 end
