54171b1390105e57589fc07b5a48bda4bb1e3681
mirion615 <mirion615@gmail.com>
2021-05-10 12:38:45 +0900

    決済機能のサーバーサイド実装
    ・ストロングパラメーター設定
    ・モデルにtokenの値を追加
    ・Gem導入
    ・決済処理記述
    ・リファクタリング
    ・バリデーション記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,7 +8,9 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
+    @item = Item.find(params[:item_id])
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -21,7 +23,16 @@ class PurchasesController < ApplicationController
   def purchase_params
     @item = Item.find(params[:item_id])
     params.require(:purchase_address).permit(:postal_code, :ship_from_id, :city, :house_number, :building_name, :phone_number).merge(
-      user_id: current_user.id, item_id: @item.id
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
     )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.item_price,
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :ship_from_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :ship_from_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
@@ -9,6 +9,7 @@ class PurchaseAddress
     validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/, message: 'Input only number' }
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :ship_from_id, numericality: { other_than: 1, message: 'Select' }
 
