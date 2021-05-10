3b411102e2f1081db28f853b50372a69363f3ac9
mirion615 <mirion615@gmail.com>
2021-05-10 16:32:21 +0900

    正しい表記に修正

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -66,12 +66,12 @@ RSpec.describe PurchaseAddress, type: :model do
         expect(@purchase_address.errors.full_messages).to include('Phone number Input correctly')
       end
       it 'phone_numberは9桁以下では保存できない' do
-        @purchase_address.phone_number = 123456789
+        @purchase_address.phone_number = '123456789'
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include('Phone number Input correctly')
       end
       it 'phone_numberは12桁以上では保存できない' do
-        @purchase_address.phone_number = 123456789012
+        @purchase_address.phone_number = '123456789012'
         @purchase_address.valid?
         expect(@purchase_address.errors.full_messages).to include('Phone number Input correctly')
       end
