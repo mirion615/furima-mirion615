c08e03c67b4bcb4d5f1ecb460cbfa87a4ffc2b0f
mirion615 <mirion615@gmail.com>
2021-05-10 12:38:56 +0900

    テストコード記述

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     house_number  { Gimei.town.kanji }
     building_name { '東京ハイツ' }
     phone_number  { Faker::Number.leading_zero_number(digits: 10) }
+    token         {"tok_abcdefghijk00000000000000000"}
   end
 end
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -11,12 +11,16 @@ RSpec.describe PurchaseAddress, type: :model do
 
     context '内容に問題がない場合' do
       it 'すべての値が正しく入力されていれば保存できること' do
-        binding.pry
         expect(@purchase_address).to be_valid
       end
     end
 
     context '内容に問題がある場合' do
+      it 'tokenが空だと保存できないこと' do
+        @purchase_address.token = nil
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Token can't be blank")
+      end
       it 'postal_codeが空だと保存できないこと' do
         @purchase_address.postal_code = ''
         @purchase_address.valid?
