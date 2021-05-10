ae1ea98f1d8980613306355700956f2748ffde85
mirion615 <mirion615@gmail.com>
2021-05-10 15:20:00 +0900

    sold_out_item追記
    購入済みの商品に対して、ログイン状態のユーザーでもトップページに戻す

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :sold_out_item, only: :edit
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -53,5 +54,10 @@ class ItemsController < ApplicationController
   def move_to_index
     @item = Item.find(params[:id])
     redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
+  end
+
+  def sold_out_item
+    @item = Item.find(params[:id])
+    redirect_to root_path if @item.purchase.present?
   end
 end
