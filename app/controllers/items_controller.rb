5922ff9e4c0fa830ca8c28da59c71e70bb767166
mirion615 <mirion615@gmail.com>
2021-05-10 15:44:48 +0900

    コードの記述削除
    before_action :authenticate_user!と被っている

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,7 +52,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def sold_out_item
