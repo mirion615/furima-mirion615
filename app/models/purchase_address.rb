caa7140d5ced0091b833fdd94f64c8dce6265031
mirion615 <mirion615@gmail.com>
2021-05-10 16:30:46 +0900

    不要なコードを修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -15,7 +15,7 @@ class PurchaseAddress
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    address = Address.create(postal_code: postal_code, city: city, house_number: house_number, building_name: building_name,
+    Address.create(postal_code: postal_code, city: city, house_number: house_number, building_name: building_name,
                              phone_number: phone_number, ship_from_id: ship_from_id, purchase_id: purchase.id)
   end
 end
